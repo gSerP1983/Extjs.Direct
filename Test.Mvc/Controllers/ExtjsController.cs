6258d199868e7f1026d15880c9bfd54df2bc505e
Sergey Petrov <gserp1983@gmail.com>
2016-07-07 18:34:34 +0300

    add Test.Mvc

---
+++ Test.Mvc/Controllers/ExtjsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Web.Mvc;
+using Extjs.Direct;
+using Extjs.Direct.Extension;
+using Test.Mvc.Util;
+
+namespace Test.Mvc.Controllers
+{
+    public class ExtjsController : Controller
+    {
+        protected override IAsyncResult BeginExecuteCore(AsyncCallback callback, object state)
+        {
+            // enable CORS
+            ExtjsControllerHelper.EnableCors(HttpContext);
+            // disable Redirect
+            Response.SuppressFormsAuthenticationRedirect = true;
+
+            return base.BeginExecuteCore(callback, state);
+        }
+
+        public ActionResult Meta()
+        {
+            if (Request.HttpMethod == "OPTIONS")
+                return ExtjsControllerHelper.Ok;
+
+            return Json(Executor.Instance.Meta(), JsonRequestBehavior.AllowGet);
+        }
+
+        // for OPTIONS
+        public ActionResult Rpc()
+        {
+            return ExtjsControllerHelper.Ok;
+        }
+
+        [HttpPost]
+        // ReSharper disable once UnusedParameter.Global
+        public ActionResult Rpc(object emptyParameter)
+        {
+            var request = Request.InputStream.AsString();
+            return new NewtonsoftJsonResult(Executor.Instance.Execute(request, HttpContext));
+        }
+    }
+}
---
+++ Test.Mvc/Util/NewtonsoftJsonResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web.Mvc;
+using Extjs.Direct.Extension;
+
+namespace Test.Mvc.Util
+{
+    public class NewtonsoftJsonResult : ActionResult
+    {
+        private readonly object _data;
+
+        public NewtonsoftJsonResult(object data)
+        {
+            _data = data;
+        }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException("context");
+
+            var response = context.HttpContext.Response;
+            response.Write(_data.AsJson());
+        }
+    }
+}
