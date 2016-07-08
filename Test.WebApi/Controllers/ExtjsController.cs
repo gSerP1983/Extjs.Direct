c143c0ff4dc558aa8017b31e5f04d5d6c094a4dd
Sergey Petrov <gserp1983@gmail.com>
2016-07-08 18:39:38 +0300

    add Test.WebApi

---
+++ Test.WebApi/Controllers/ExtjsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using Extjs.Direct;
+using Extjs.Direct.Extension;
+
+namespace Test.WebApi.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class ExtjsController : ApiController
+    {
+        [AcceptVerbs("GET")]
+        public string Meta()
+        {
+            return Executor.Instance.Meta().AsJson();
+        }
+
+        [AcceptVerbs("POST")]
+        public string Rpc()
+        {
+            var httpContext = (HttpContextWrapper)Request.Properties["MS_HttpContext"];
+            var request = httpContext.Request.InputStream.AsString();
+            return Executor.Instance.Execute(request, httpContext).AsJson();
+        }
+    }
+}
---
+++ Test.WebApi/Global.asax.cs
@@ -0,0 +1,16 @@
+﻿using System.Web;
+using System.Web.Http;
+using Test.Server.Base;
+
+namespace Test.WebApi
+{
+    public class WebApiApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Initializer.Initialize();
+
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
