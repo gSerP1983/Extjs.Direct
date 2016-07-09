d32f30d4e7a852edaada0d4c8485f0ea2740a71f
Sergey Petrov <gserp1983@gmail.com>
2016-07-09 14:36:05 +0400

    Dead code

--- Test.Nancy.Aspnet/Bootstrapper.cs
+++ Test.Nancy.Aspnet/Bootstrapper.cs
@@ -22,29 +22,13 @@ namespace Test.Nancy.Aspnet
             pipelines.AfterRequest.AddItemToEndOfPipeline(ctx => WithCors(ctx.Response));
         }
 
-        protected override void ConfigureConventions(NancyConventions conventions)
-        {
-            base.ConfigureConventions(conventions);
-
-            // CORS Enable
-            for (var i = 0; i < conventions.StaticContentsConventions.Count(); i++)
-            {
-                var convention = conventions.StaticContentsConventions[i];
-                conventions.StaticContentsConventions[i] = (ctx, root) =>
-                {
-                    var response = convention.Invoke(ctx, root);
-                    return WithCors(response);
-                };
-            }
-        }
-
         private static Response WithCors(Response response)
         {
             if (response == null)
                 return null;
 
             return response
-                .WithHeader("Access-Control-Allow-Origin", "*") // вместо '*' можно прочесть из ctx.Request.Headers["Origin"].FirstOrDefault()
+                .WithHeader("Access-Control-Allow-Origin", "*")
                 .WithHeader("Access-Control-Allow-Methods", "POST,GET")
                 .WithHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
         }        
